660c6299e858398f7076767bacbcfcb4159f15b1
Aditya Kauthkar <adityakauthkar2021.it@mmcoe.edu.in>
2026-01-19 17:38:04 +0000

    Fresh start backend

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const  connectDB = require('./config/database');
+
+require('dotenv').config();
+
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.get('/' , (req , res)=>{
+    res.send("Server running ... ");
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+
